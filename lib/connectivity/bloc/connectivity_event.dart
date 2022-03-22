90e3d1544ed2a962af6a38b84d917e013fa24822
Sofia Toro Chambi <sofie.torch@outlook.com>
2022-03-22 15:56:16 -0400

    add class reference to doc comments on connectivity events

--- lib/connectivity/bloc/connectivity_event.dart
+++ lib/connectivity/bloc/connectivity_event.dart
@@ -1,22 +1,25 @@
 part of 'connectivity_bloc.dart';
 
-/// Base class for Connectivity Bloc Events.
+/// Base class for [ConnectivityBloc] events.
 abstract class ConnectivityEvent {
   const ConnectivityEvent();
 }
 
+/// Event of [ConnectivityBloc].
 /// Indicates that a listening request of
 /// connectivity changes was sent.
 class ConnectivityRequested extends ConnectivityEvent {
   const ConnectivityRequested();
 }
 
+/// Event of [ConnectivityBloc]
 /// Indicates that the internet connection
 /// has been lost, so it is unavailable.
 class ConnectivityLost extends ConnectivityEvent {
   const ConnectivityLost();
 }
 
+/// Event of [ConnectivityBloc]
 /// Indicates that the internet connection
 /// has been retrieved, so it is available.
 class ConnectivityRetrieved extends ConnectivityEvent {
