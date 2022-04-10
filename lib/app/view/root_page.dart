3f7860c8ce2fc224a2c010dea32083c7084e4759
Sofia Toro <sofie.torch@outlook.com>
2022-04-09 20:13:14 -0400

    refactor app view and widgets file structure

--- lib/app/view/root_page.dart
+++ lib/app/view/root_page.dart
@@ -18,59 +18,16 @@ class RootPage extends StatelessWidget {
 /// The main view for the app,
 /// contains the needed structure to navigate through
 /// the app and visualize pages.
-/// Listens to [NavigationCubit] for navigation management.
+/// Listens to [NavigationCubit] for navigation management
+/// on bottom navigation bar.
 class _RootView extends StatelessWidget {
   const _RootView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final l10n = context.l10n;
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          children: [
-            const Icon(AppIcons.university),
-            const SizedBox(width: 8),
-            Text(l10n.university),
-          ],
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: IconButton(
-              icon: const Icon(MdiIcons.menu),
-              onPressed: () => showModalBottomSheet<Widget>(
-                context: context,
-                builder: (_) => const BottomSheetMenu(),
-              ),
-            ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: const Icon(MdiIcons.homeVariantOutline),
-            label: l10n.navItemHome,
-          ),
-          BottomNavigationBarItem(
-            icon: const Icon(MdiIcons.school),
-            label: l10n.navItemAcademic,
-          ),
-          BottomNavigationBarItem(
-            icon: const Icon(MdiIcons.cashMultiple),
-            label: l10n.navItemPayments,
-          ),
-          BottomNavigationBarItem(
-            icon: const Icon(MdiIcons.accountCircle),
-            label: l10n.navItemProfile,
-          ),
-        ],
-        onTap: context.read<NavigationCubit>().changePage,
-        currentIndex: context.select(
-          (NavigationCubit cubit) => cubit.state.index,
-        ),
-      ),
+      appBar: _buildAppBar(context),
+      bottomNavigationBar: const AppBottomNavigationBar(),
       body: BlocBuilder<NavigationCubit, NavigationState>(
         builder: (_, state) {
           if (state is NavigationAcademic) return const HomePage();
@@ -79,6 +36,36 @@ class _RootView extends StatelessWidget {
           return const HomePage();
         },
       ),
+    );
+  }
+
+  AppBar _buildAppBar(BuildContext context) {
+    final l10n = context.l10n;
+    return AppBar(
+      foregroundColor: AppColors.shiraz,
+      iconTheme: Theme.of(context)
+          .appBarTheme
+          .iconTheme
+          ?.copyWith(color: AppColors.shiraz),
+      title: Row(
+        children: [
+          const Icon(AppIcons.university),
+          const SizedBox(width: 8),
+          Text(l10n.university),
+        ],
+      ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: IconButton(
+            icon: const Icon(MdiIcons.menu),
+            onPressed: () => showModalBottomSheet<Widget>(
+              context: context,
+              builder: (_) => const BottomSheetMenu(),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
