ff5e2b81b23f448594bc6446811deabc20eb2f01
AnthonyVocal <jorelvocalhurtado@gmail.com>
2022-06-07 10:15:54 -0400

    terminado despliegue de lugares de pagos

--- lib/payment_places/widgets/payment_places.dart
+++ lib/payment_places/widgets/payment_places.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:univalle_app/environment.dart';
+import 'package:univalle_app/models/payment_places.dart';
 import 'package:univalle_app/payment_places/bloc/payment_places_bloc.dart';
 import 'package:univalle_app/payment_places/widgets/cubit/payment_item_cubit.dart';
 import 'package:univalle_app/theme/app_colors.dart';
@@ -65,7 +67,7 @@ class PaymentPlacesView extends StatelessWidget {
               ),
             ],
           ),
-          const SizedBox(height: 4),
+          //const SizedBox(height: 2),
           if (showPayments) const BancosList(),
         ],
       ),
@@ -75,17 +77,64 @@ class PaymentPlacesView extends StatelessWidget {
 
 class BancosList extends StatelessWidget {
   const BancosList({Key? key}) : super(key: key);
-
+  //final PaymentsPlaces type;
   @override
   Widget build(BuildContext context) {
     final payments = context.read<PaymentPlacesBloc>().state.paymentplaces;
-    return ListView.builder(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 18,
-      ),
-      itemCount: payments.length,
-      shrinkWrap: true,
-      itemBuilder: (_, index) => Text(payments[index].name),
+
+    //Implementacion
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Chip(
+              label: Text(
+                'Bancos',
+                style: Theme.of(context).textTheme.overline,
+              ),
+              backgroundColor: AppColors.shiraz.shade100,
+            ),
+            Chip(
+              label: Text(
+                'Cajas',
+                style: Theme.of(context).textTheme.overline,
+              ),
+              backgroundColor: AppColors.gray.shade200,
+            ),
+          ],
+        ),
+        const SizedBox(height: 4),
+        ListView.builder(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 18,
+          ),
+          itemCount: payments.length,
+          shrinkWrap: true,
+          itemBuilder: (_, index) {
+            final paymentPlace = payments[index];
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    SizedBox(
+                      width: 30,
+                      child: Image.network(payments[index].logoImageUrl),
+                    ),
+                    const SizedBox(width: 5),
+                    Text(payments[index].name),
+                  ],
+                ),
+                Text(paymentPlace.type.join(', ')),
+              ],
+            );
+          },
+        ),
+      ],
     );
   }
 }
